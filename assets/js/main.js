a8a43f83a5510607524a0ab9cdef92389fc2b12d
Marcelo_Gabriel_Pesoa <pesoa01@gmail.com>
2023-03-12 10:35:00 -0300

    Starting html...

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,40 @@ async function getProducts() {
         const res = await data.json();
 
         window.localStorage.setItem("products", JSON.stringify(res)); //Se crea el localStorge de nombre products
-        console.log("res:", typeof(res) , res );
+        
+        //console.log("res:", typeof(res) , res );
         return res;
     } catch (error) {
         console.log(error);
     }
 }
 
+
+function printProducts(db) {
+    let html = '';
+    const productsHTML = document.querySelector(".products");
+    for (const product of db.products) {
+     // console.log(product);
+        html +=`
+            <div class="product">
+                <p>${product.name}</p>
+            </div>
+        `;
+    }
+    productsHTML.innerHTML = html;
+}
+
+
 async function main() {
-
-    const productsData = JSON.parse(window.localStorage.getItem("products")) || await getProducts();
-
-    console.log('window.localStorage.getItem("products"): ', JSON.parse(window.localStorage.getItem("products")) ); //Se obtiene (get) el localStorge de nombre products
-
-
-    console.log("productsData:=> " , typeof(productsData) ,productsData);
-
+    const db = {
+        products: JSON.parse(window.localStorage.getItem("products")) || (await getProducts()),
+        cart: {},
+    };
+    
+      console.log("db.products Data:=> " , typeof(db.products) ,db.products);
+      
+      printProducts(db);
+    
 }
 
 main();
